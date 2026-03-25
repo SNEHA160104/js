59a21549250f1370283e52f85cf7666668041ddb
SNEHA160104 <150802595+SNEHA160104@users.noreply.github.com>
2026-03-25 09:56:16 +0000

    converion in data types

---
+++ .github/01_basics/03_conversion.js
@@ -0,0 +1,25 @@
+let age=12
+let age1="12"
+let name="shreya"
+
+console.log(typeof age)
+console.log(typeof name)
+
+
+let valnum=Number(age1)
+console.log(typeof valnum)
+console.log(valnum)
+
+let valnum1=Number(name)
+console.log(typeof valnum1)
+console.log(valnum1)//will get nan for non numeric string,name,boolean,undefined,null
+
+let isstudent=1
+console.log(typeof isstudent)
+//1=true,0=false
+//"",0,false,null,undefined=all are false
+//"sneha"gives true
+boolisstudent=Boolean(isstudent)
+console.log(typeof boolisstudent)
+console.log(boolisstudent)
+
