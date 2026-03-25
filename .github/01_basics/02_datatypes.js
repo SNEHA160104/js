b3bb563fd1d33ceecc475e705a8b35237af157d9
SNEHA160104 <150802595+SNEHA160104@users.noreply.github.com>
2026-03-25 09:39:10 +0000

    datatypes

---
+++ .github/01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";   //treat all js code as newer version
+// alert("Hello World!");   //we are using node.js not browsers
+
+console.log(3+3)
+console.log("lalalala") // better practice to use console.log instead of alert in node.js environment 
+// code readability should be highly considered when writing code, so that other developers can easily understand it.
+let name="sneha"
+let age=22
+let isStudent=true
+
+//number=>2^53-1
+//bigint
+//string ""
+//boolean=>true/false
+//null=>standalone value that represents "nothing"
+//undefined=>a variable that has been declared but not assigned a value yet
+//symbol=>used to create unique identifiers for objects
+
+
+//object=>
+
+console.log(typeof object)
+console.log(typeof null)
+console.log(typeof undefined)
