682cda22d37bedd8b7671a748a2b2be622da4a15
SNEHA160104 <150802595+SNEHA160104@users.noreply.github.com>
2026-03-25 10:47:00 +0000

    conversions and how operators work

---
+++ .github/01_basics/04_operations.js
@@ -0,0 +1,21 @@
+let val=16
+let val2=-val
+console.log(val2) // -16
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2) // remainder of 2 divided by 2
+console.log(2**2) // 2 to the power of 2
+
+let str1="sneha"
+let str2="gupta"
+console.log(str1+str2) // concatenation of strings
+console.log(str1+" "+str2) // concatenation with space
+
+console.log("1"+3)
+console.log(1+"3")
+console.log("1"+3+4) // "134" because of left to right evaluation
+console.log(1+"3"+4) // "134" because of left to right evaluation
+console.log(1+3+"4") // "44" because 1+3 is evaluated first to 4, then concatenated with "4"
+
